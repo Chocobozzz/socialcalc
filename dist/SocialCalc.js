3d07bba7f9e2128a43cf939c799782964ac53a83
Chocobozzz <me@florianbigard.com>
2020-03-27 09:19:29 +0100

    Compat with framacalc threads

--- dist/SocialCalc.js
+++ dist/SocialCalc.js
@@ -15,6 +15,138 @@
         root.SocialCalc = factory.call(root, this);
   }
 }(this, function (window) {
+  if (typeof alert !== 'function') {
+    alert = function () {}
+  }
+
+  if (typeof document === 'undefined' || !document) {
+    var Node
+    Node = (function () {
+      Node.displayName = 'Node'
+      var prototype = Node.prototype, constructor = Node
+
+      function Node (tag, attrs, style, elems, raw) {
+        this.tag = tag != null ? tag : 'div'
+        this.attrs = attrs != null
+          ? attrs
+          : {}
+        this.style = style != null
+          ? style
+          : {}
+        this.elems = elems != null
+          ? elems
+          : []
+        this.raw = raw != null ? raw : ''
+      }
+
+      Object.defineProperty(Node.prototype, 'id', {
+        set: function (id) {
+          this.attrs.id = id
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'width', {
+        set: function (width) {
+          this.attrs.width = width
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'height', {
+        set: function (height) {
+          this.attrs.height = height
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'className', {
+        set: function ($class) {
+          this.attrs['class'] = $class
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'colSpan', {
+        set: function (colspan) {
+          this.attrs.colspan = colspan
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'rowSpan', {
+        set: function (rowspan) {
+          this.attrs.rowspan = rowspan
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'title', {
+        set: function (title) {
+          this.attrs.title = title
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'innerHTML', {
+        set: function (raw) {
+          this.raw = raw
+        },
+        get: function () {
+          var e
+          return this.raw || (function () {
+            var i$, ref$, len$, results$ = []
+            for (i$ = 0, len$ = (ref$ = this.elems).length; i$ < len$; ++i$) {
+              e = ref$[i$]
+              results$.push(e.outerHTML)
+            }
+            return results$
+          }.call(this)).join('\n')
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Object.defineProperty(Node.prototype, 'outerHTML', {
+        get: function () {
+          var tag, attrs, style, css, k, v
+          tag = this.tag, attrs = this.attrs, style = this.style
+          css = style.cssText || (function () {
+            var ref$, results$ = []
+            for (k in ref$ = style) {
+              v = ref$[k]
+              results$.push(k.replace(/[A-Z]/g, '-$&').toLowerCase() + ':' + v)
+            }
+            return results$
+          }()).join(';')
+          if (css) {
+            attrs.style = css
+          } else {
+            delete attrs.style
+          }
+          return '<' + tag + (function () {
+            var ref$, results$ = []
+            for (k in ref$ = attrs) {
+              v = ref$[k]
+              results$.push(' ' + k + '="' + v + '"')
+            }
+            return results$
+          }()).join('') + '>' + this.innerHTML + '</' + tag + '>'
+        },
+        configurable: true,
+        enumerable: true
+      })
+      Node.prototype.appendChild = function (it) {
+        return this.elems.push(it)
+      }
+      return Node
+    }())
+
+    document = {
+      createElement: function (it) {
+        return new Node(it)
+      }
+    }
+  }
 
 //
 /*
@@ -27636,7 +27768,7 @@ str = str.replace(/([^\n])\r([^\n])/g, "$1\r\n$2");
 // END OF FILE
 
 
-if('undefined' === typeof document) {
+if('undefined' === typeof document || typeof document.getElementById !== 'function') {
     // We don't really need a DOM-based presentation layer on the server
     SocialCalc.GetEditorCellElement = function () {};
     SocialCalc.ReplaceCell = function () {};
